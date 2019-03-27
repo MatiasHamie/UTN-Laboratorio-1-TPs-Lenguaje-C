6003aa67d04981fdd23dac724546bc30dd3c90f6
matiashamie <mnhamie@gmail.com>
2019-03-27 15:33:11 -0300

    Falta el factorial, y ordenar las funciones MENU-MAIN

---
+++ TP1/Hamie.h
@@ -0,0 +1,46 @@
+/** \brief Invoca un menu que se actualiza segun como esten los flags
+ *
+ *
+ * \return No devuelve nada, el mismo invoca a otras funciones aritmeticas
+ */
+void menu();
+
+/** \brief Ingresando 2 numeros float los suma
+ *
+ * \param numeroUno float
+ * \param numeroDos float
+ *
+ * \return Regresa el resultado tipo FLOAT
+ *
+ */
+ float sumaDosNumeros(float numeroUno, float numeroDos);
+
+/** \brief Ingresando 2 numeros float los resta
+ *
+ * \param numeroUno float
+ * \param numeroDos float
+ *
+ * \return Regresa el resultado tipo FLOAT
+ *
+ */
+float restaDosNumeros(float numeroUno, float numeroDos);
+
+/** \brief Ingresando 2 numeros float los multiplica
+ *
+ * \param numeroUno float
+ * \param numeroDos float
+ *
+ * \return Regresa el resultado tipo FLOAT
+ *
+ */
+float multiplicacionDosNumeros(float numeroUno, float numeroDos);
+
+/** \brief Ingresando 2 numeros float los divide
+ *
+ * \param numeroUno float
+ * \param numeroDos float
+ *
+ * \return Regresa el resultado tipo FLOAT
+ *
+ */
+float divisionDosNumeros(float numeroUno, float numeroDos);
